4d81de5f40dcd78e22e8285d4e13f56704ef0c43
matiiezequiiel <matii.ezequiiel@gmail.com>
2020-04-13 20:56:04 -0300

    13/4/2020 20:55

--- TP_LABORATORIO_1/funciones.c
+++ TP_LABORATORIO_1/funciones.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+
+int mostrarMenu(char* numeroMenuX ,char* numeroMenuY )
+{
+
+        int opcion;
+
+        printf("BIENVENIDOS A LA CALCULADORA!!\n\n");
+        printf("1. Ingresar 1er operando (A=%s)",numeroMenuX);
+        printf("\n2. Ingresar 2do operando (B=%s)", numeroMenuY);
+        printf("\n3. Calcular todas las operaciones.");
+        printf("\n  A). Calcular la suma (A+B)");
+        printf("\n  B). Calcular la resta (A-B)");
+        printf("\n  C). Calcular la division (A/B)");
+        printf("\n  D). Calcular la multiplicacion (A*B)");
+        printf("\n  E). Calcular el factorial (A!)");
+        printf("\n4. Informar resultados.");
+        printf("\n  A). El resultado de A+B es:");
+        printf("\n  B). El resultado de A-B es:");
+        printf("\n  C). El resultado de A/B es:");
+        printf("\n  D). El resultado de A/B es:");
+        printf("\n  E). El factorial de A es: r y El factorial de B es: r2");
+        printf("\n5. Salir");
+
+        printf("\n\nOpcion elegida:");
+        scanf("%d",& opcion);
+        system("cls");
+        return opcion;
+
+}
--- TP_LABORATORIO_1/main.c
+++ TP_LABORATORIO_1/main.c
@@ -1,11 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 #include "funciones.h"
 
 
-//PRUEBA GITHUB
+/*
+1. Ingresar 1er operando (A=x)
+2. Ingresar 2do operando (B=y)
+3. Calcular todas las operaciones
+a) Calcular la suma (A+B)
+b) Calcular la resta (A-B)
+c) Calcular la division (A/B)
+d) Calcular la multiplicacion (A*B)
+e) Calcular el factorial (A!)
+4. Informar resultados
+a) “El resultado de A+B es: r”
+b) “El resultado de A-B es: r”
+c) “El resultado de A/B es: r” o “No es posible dividir por cero”
+d) “El resultado de A*B es: r”
+e) “El factorial de A es: r1 y El factorial de B es: r2”
+5. Salir
+*/
+
 int main()
 {
-    printf("Hello world!\n");
+    int opcion;
+    float primerOperando;
+    float segundoOperando;
+    char numeroMenuX[10]="X";
+    char numeroMenuY[10]="Y";
+    char respuesta=' ';
+    float resultadoSuma;
+    float resultadoResta;
+    float resultadoMultiplicacion;
+    float resultadoDivision;
+    unsigned long long int resultadoFactorial;
+    int auxFactorial;
+
+
+    do
+    {
+        opcion=mostrarMenu(numeroMenuX,numeroMenuY);
+
+
+
+    }while(opcion!=5);
+
     return 0;
 }
