768a8672e8575d3cd24fb92eb68e039d8758e401
matiiezequiiel <matii.ezequiiel@gmail.com>
2020-06-22 10:01:45 -0300

    Pruebas TP3

--- TP_LABORATORIO_3/Funciones.c
+++ TP_LABORATORIO_3/Funciones.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "Funciones.h"
 #include "Validaciones.h"
 #include <stdio.h>
@@ -35,41 +34,3 @@ int mostrarMenuABM(void)
 
 }
 
-=======
-#include "Funciones.h"
-#include "Validaciones.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-int mostrarMenuABM(void)
-{
-        char entrada[10];
-        int opcion;
-
-
-        printf("BIENVENIDOS: \n\n");
-        printf("1. Cargar los datos de los empleados desde el archivo data.csv (modo texto).");
-        printf("\n2. Cargar los datos de los empleados desde el archivo data.csv (modo binario). ");
-        printf("\n3. Alta de empleado. ");
-        printf("\n4. Modificar datos de empleado.");
-        printf("\n5. Baja de empleado.");
-        printf("\n6. Listar empleados. ");
-        printf("\n7. Ordenar empleados. ");
-        printf("\n8. Guardar los datos de los empleados en el archivo data.csv (modo texto).");
-        printf("\n9. Guardar los datos de los empleados en el archivo data.csv (modo binario).");
-        printf("\n10. Salir.");
-
-        printf("\n\nOpcion elegida:");
-        fflush(stdin);
-        gets(entrada);
-
-        opcion=validarIntEntreRangos(entrada,1,10);
-        system("cls");
-        return opcion;
-
-
-}
-
->>>>>>> 765b5f569e3eca1e3793ba023b5893c9a36aa7d9
--- TP_LABORATORIO_3/Funciones.h
+++ TP_LABORATORIO_3/Funciones.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** \brief Muestra menu para ABM
  *
  * \param void
@@ -6,12 +5,3 @@
  *
  */
 int mostrarMenuABM(void);
-=======
-/** \brief Muestra menu para ABM
- *
- * \param void
- * \return int Opcion.
- *
- */
-int mostrarMenuABM(void);
->>>>>>> 765b5f569e3eca1e3793ba023b5893c9a36aa7d9
